95cfa7a164af1c94b628f166ceba7e5112f798b5
Jfnrosimo <j.rosimo098@gmail.com>
2023-05-03 08:47:56 +0800

    Feat: add game session class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,16 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Models.Player();
+            CurrentPlayer.Name = "Jeff";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
