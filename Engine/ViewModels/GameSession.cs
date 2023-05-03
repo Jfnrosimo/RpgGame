994b72b45d4efd9f5c0d918ed2a34617b681b883
Jfnrosimo <j.rosimo098@gmail.com>
2023-05-03 17:12:26 +0800

    Feat: added engine folder

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,16 +1,37 @@
-﻿using Engine.Models;
+﻿using Engine.Factories;
+using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer
+        {
+            get; set;
+        }
+        public Location CurrentLocation
+        {
+            get; set;
+        }
+        public World CurrentWorld
+        {
+            get; set;
+        }
 
         public GameSession()
         {
-            CurrentPlayer = new Models.Player();
+            CurrentPlayer = new Player();
             CurrentPlayer.Name = "Jeff";
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.HitPoints = 4;
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 10;
+            CurrentPlayer.Level = 1;
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0,0);
         }
     }
 }
